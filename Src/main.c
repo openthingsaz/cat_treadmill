78ce5ecf50fc90ffe1ab41443a783c7c5cf435fa
openthingsaz <openthingsaz@gmail.com>
2019-12-14 11:16:13 +0900

    1초 마다 증강하는 타이머 카운터 수정
    이 함수는 1초 마다 증가하는 타이머로 현재 타임을 얻는데 사용될 예정이다.

--- Src/main.c
+++ Src/main.c
@@ -268,19 +268,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE BEGIN Callback 1 */
   if (htim->Instance == TIM11)
   {
-    if ( ledPos_before == ledPosTmp) 
-    {
-      timecnt++;
-      if (timecnt >= OFF_TIME) 
-      {
-        timecnt = 0;
-        set_sleep();
-      }
-    }
-    else 
-    {
-      ledPosTmp = ledPos_before;
-    }
+    timecnt++;
   }
 
   /* USER CODE END Callback 1 */
