4ef0e8caf7186cd155a12371e12ed68b5bfb2918
openthingsaz <openthingsaz@gmail.com>
2019-12-19 16:21:01 +0900

    파워 온오프 시 블루투스 제어
    파워 온오프 시 블루투스 Enable, Disable 제어 추가

--- Src/power.c
+++ Src/power.c
@@ -9,6 +9,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "power.h"
 #include <stdbool.h>
+#include "mdbt42q.h"
 
 uint8_t bat_val = 0;
 
@@ -19,18 +20,18 @@ void power_en(void)
   HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_RESET); // PERI_3V3_PWR_nEN, Gyroscope, Accelerometer, High Disable, Low Enable, Device : U2
   HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_SET); // LED_LMIT_EN, High Enable, Low Disable, Device : U5
   HAL_GPIO_WritePin(LED_3V3_PWR_nEN_GPIO_Port, LED_3V3_PWR_nEN_Pin, GPIO_PIN_SET); // LED_3V3_PWR_nEN, High Enable, Low Disable, Device : U3
+  ble_enable();
 }
 
 /* power Disable */
 void power_dis(void)
 {
   // power Controler
-  // HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_SET); // PERI_3V3_PWR_nEN, Gyroscope, Bluetooth, High Disable, Low Enable
+  HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_SET); // PERI_3V3_PWR_nEN, Gyroscope, Bluetooth, High Disable, Low Enable
   HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_RESET); // LED_LMIT_EN, High Enable, Low Disable
   HAL_GPIO_WritePin(LED_3V3_PWR_nEN_GPIO_Port, LED_3V3_PWR_nEN_Pin, GPIO_PIN_RESET); // LED_3V3_PWR_nEN, High Enable, Low Disable
+  ble_disable();
 }
-
-
 
 void led_power_off(void)
 {
