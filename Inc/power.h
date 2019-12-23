d999254f9912baa7974e92241bc3684f2d726a7b
openthingsaz <openthingsaz@gmail.com>
2019-12-23 17:06:53 +0900

    power 제어 함수 수정 및 power_mode에 따른 제어 추가
    power_mode에 따라서 power off방법 변경됨
    기본 performace모드에서는 블루투스와 자이로센서를 off하지 않는다.

--- Inc/power.h
+++ Inc/power.h
@@ -20,10 +20,18 @@
 #include "main.h"
 #include <stdbool.h>
 
+#define POWR_PERFORMANCE 0x01
+#define POWR_SAVE 0x02
+
 void power_en(void);
 void power_dis(void);
 void led_power_off(void);
 void led_power_on(void);
+void set_power_mode(uint8_t mode);
+uint8_t get_power_mode(void);
+void set_wakeup(void);
+void set_sleep(void);
+
 extern uint8_t bat_val;
 extern uint8_t power_mode;
 
--- Src/power.c
+++ Src/power.c
@@ -12,27 +12,7 @@
 #include "mdbt42q.h"
 
 uint8_t bat_val = 0;
-uint8_t power_mode = 0;
-
-/* power Enable */
-void power_en(void)
-{
-  // power Controler
-  HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_RESET); // PERI_3V3_PWR_nEN, Gyroscope, Accelerometer, High Disable, Low Enable, Device : U2
-  HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_SET); // LED_LMIT_EN, High Enable, Low Disable, Device : U5
-  HAL_GPIO_WritePin(LED_3V3_PWR_EN_GPIO_Port, LED_3V3_PWR_EN_Pin, GPIO_PIN_SET); // LED_3V3_PWR_nEN, High Enable, Low Disable, Device : U3
-  ble_enable();
-}
-
-/* power Disable */
-void power_dis(void)
-{
-  // power Controler
-  HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_SET); // PERI_3V3_PWR_nEN, Gyroscope, Bluetooth, High Disable, Low Enable
-  HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_RESET); // LED_LMIT_EN, High Enable, Low Disable
-  HAL_GPIO_WritePin(LED_3V3_PWR_EN_GPIO_Port, LED_3V3_PWR_EN_Pin, GPIO_PIN_RESET); // LED_3V3_PWR_nEN, High Enable, Low Disable
-  ble_disable();
-}
+uint8_t power_mode = POWR_PERFORMANCE;
 
 void led_power_off(void)
 {
@@ -41,8 +21,62 @@ void led_power_off(void)
 
 void led_power_on(void)
 {
-  HAL_GPIO_WritePin(LED_3V3_PWR_EN_GPIO_Port, LED_3V3_PWR_EN_Pin, GPIO_PIN_SET); // LED_3V3_PWR_nEN, High Enable, Low Disable
+  HAL_GPIO_WritePin(LED_3V3_PWR_EN_GPIO_Port, LED_3V3_PWR_EN_Pin, GPIO_PIN_SET); // LED_3V3_PWR_nEN, High Enable, Low Disable, Device : U3
+}
+
+void peri_on(void)
+{
+  HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_RESET); // PERI_3V3_PWR_nEN, Gyroscope, Accelerometer, High Disable, Low Enable, Device : U2
+}
+
+void peri_off(void)
+{
+  HAL_GPIO_WritePin(PERI_3V3_PWR_nEN_GPIO_Port, PERI_3V3_PWR_nEN_Pin, GPIO_PIN_SET); // PERI_3V3_PWR_nEN, Gyroscope, Bluetooth, High Disable, Low Enable
+}
+/* power Enable */
+void power_en(void)
+{
+  // power Controler
+  peri_on();
+  HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_SET); // LED_LMIT_EN, High Enable, Low Disable, Device : U5
+  led_power_on(); 
+  ble_enable();
+}
+
+/* power Disable */
+void power_dis(void)
+{
+  // power Controler
+  HAL_GPIO_WritePin(LED_LMIT_EN_GPIO_Port, LED_LMIT_EN_Pin, GPIO_PIN_RESET); // LED_LMIT_EN, High Enable, Low Disable
+  led_power_off();
+  if (power_mode == POWR_SAVE) 
+  {
+    ble_disable();
+    peri_off();
+  }
+}
+
+uint8_t get_power_mode(void)
+{
+  return power_mode;
+}
+
+void set_power_mode(uint8_t mode)
+{
+  power_mode = mode;
 }
 
 
+void set_wakeup(void) 
+{
+  power_en();
+  running_mode = STAT_RUNNING;
+  //HAL_TIM_Base_Start_IT(&htim11);
+}
 
+void set_sleep(void) 
+{
+  power_dis();
+  running_mode = STAT_SLEEP;
+  //HAL_TIM_Base_Stop_IT(&htim11);
+}
